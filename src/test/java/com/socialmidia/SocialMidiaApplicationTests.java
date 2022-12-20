b6a0ec9c496f1453e14330f24c3e467fd17092e3
@harishj.avertatech <harishjaybhaye510@gmail.com>
2022-12-20 22:01:43 +0530

    new pro

---
+++ src/test/java/com/socialmidia/SocialMidiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialmidia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMidiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
